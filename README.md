fea155a49f648bd7b0a3d02747b15d2b5c63a3c9
ZhangZhiQiang <zhangzhiqiang@pegasus-mobile.com>
2017-04-14 18:38:32 +0800

    优化ArcBarView

--- README.md
+++ README.md
@@ -7,33 +7,35 @@ ArcBarView是一个底部具有弧线形的一个自定义View。
 
 #### ArcBarView支持的属性：
 ```
-<declare-styleable name="ArcBarView">
+ <declare-styleable name="ArcBarView">
+ 
+        <!-- arcBarHightToScreenPercentage：ArcBarView的高度和屏幕的高度比例 -->
+        <attr name="arcBarHightToScreenPercentage" format="float" />
 
-    <!-- arcBarHightToScreenPercentage：ArcBarView的高度和屏幕的高度比例 -->
-    <attr name="arcBarHightToScreenPercentage" format="float" />
-    
-    <!-- arcBarHightToArcPercentage:椭圆的高度一半与ArcBarView的高度比例 -->
-    <attr name="arcBarHightToArcPercentage" format="float" />
-    
-    <!-- arcBarWidthToArcPercentage:椭圆宽度超出屏幕的总距离的一半与屏幕宽度的比例 -->
-    <attr name="arcBarWidthToArcPercentage" format="float" />
-    
-    <!-- 开始的渐变色 -->
-    <attr name="startColor" format="color" />
-    
-    <!--中间的渐变色-->
-    <attr name="centerColor" format="color" />
-    
-    <!-- 最后的渐变色 -->
-    <attr name="endColor" format="color" />
-    
-    <!--   注意：使用渐变色最少有2个色值   -->
-    
-</declare-styleable>
+        <!-- arcBarHightToArcPercentage:椭圆的高度一半与ArcBarView的高度比例 -->
+        <attr name="arcBarHightToArcPercentage" format="float" />
+
+        <!-- arcBarWidthToArcPercentage:椭圆宽度超出屏幕的总距离的一半与屏幕宽度的比例 -->
+        <attr name="arcBarWidthToArcPercentage" format="float" />
+
+        <!-- ArcBarView的color颜色 -->
+        <attr name="arcBarColor" format="color" />
+
+        <!--是否显示渐变色，如果设置为true，那么arcBarColor属性无效，而且至少使用2种渐变色-->
+        <attr name="arcBarShowGradientColor" format="boolean" />
+        
+        <!-- 开始的渐变色 -->
+        <attr name="arcBarStartGradientColor" format="color" />
+        <!--中间的渐变色-->
+        <attr name="arcBarCenterGradientColor" format="color" />
+        <!-- 最后的渐变色 -->
+        <attr name="arcBarEndGradientColor" format="color" />
+    </declare-styleable>
 ```
 
 #### ArcBarView的使用
 ```
+<?xml version="1.0" encoding="utf-8"?>
 <RelativeLayout xmlns:android="http://schemas.android.com/apk/res/android"
     xmlns:app="http://schemas.android.com/apk/res-auto"
     xmlns:tools="http://schemas.android.com/tools"
@@ -41,16 +43,16 @@ ArcBarView是一个底部具有弧线形的一个自定义View。
     android:layout_height="match_parent"
     tools:context="com.yundoku.arcbarview.MainActivity">
 
-    <com.yundoku.arcbarview.view.ArcBarView 
-        xmlns:app="http://schemas.android.com/apk/res-auto"
+    <com.yundoku.arcbarview.view.ArcBarView xmlns:app="http://schemas.android.com/apk/res-auto"
         android:layout_width="match_parent"
         android:layout_height="match_parent"
+        app:arcBarCenterGradientColor="@color/colorPrimary"
+        app:arcBarEndGradientColor="@color/colorPrimaryDark"
         app:arcBarHightToArcPercentage="0.2"
         app:arcBarHightToScreenPercentage="0.4"
-        app:arcBarWidthToArcPercentage="0.1"
-        app:centerColor="@color/colorPrimary"
-        app:endColor="@color/colorPrimaryDark"
-        app:startColor="@color/colorAccent" />
+        app:arcBarShowGradientColor="true"
+        app:arcBarStartGradientColor="@color/colorAccent"
+        app:arcBarWidthToArcPercentage="0.1" />
 
 </RelativeLayout>
 ```
--- app/src/main/java/com/yundoku/arcbarview/view/ArcBarView.java
+++ app/src/main/java/com/yundoku/arcbarview/view/ArcBarView.java
@@ -13,7 +13,6 @@ import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
 
-
 import com.yundoku.arcbarview.R;
 
 import java.util.ArrayList;
@@ -26,13 +25,19 @@ import java.util.List;
 public class ArcBarView extends View {
 
 
-    private int startColor = 0xff082255;
-    private int centerColor = 0xff0c5bb9;
-    private int endColor = 0xff298DF5;
+    private int startGradientColor = 0xff082255;
+    private int centerGradientColor = 0xff0c5bb9;
+    private int endGradientColor = 0xff298DF5;
+    private int arcBarColor = 0xffffffff;
     private List<Integer> list = new ArrayList<>();
-    private int[] defaultColors = new int[]{startColor, centerColor, endColor};
+    private int[] defaultColors = new int[]{startGradientColor, centerGradientColor, endGradientColor};
     private Paint paint;
     private Path path;
+    private boolean isShowGradientColor;
+
+    private float arcBarHightToScreenPercentage = 0.0f;
+    private float arcBarHightToArcPercentage = 0.0f;
+    private float arcBarWidthToArcPercentage = 0.0f;
 
     public ArcBarView(Context context) {
         super(context);
@@ -41,17 +46,17 @@ public class ArcBarView extends View {
     public ArcBarView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
         TypedArray typedArray = null;
-        float arcBarHightToScreenPercentage = 0.0f;
-        float arcBarHightToArcPercentage = 0.0f;
-        float arcBarWidthToArcPercentage = 0.0f;
+
         try {
             typedArray = context.obtainStyledAttributes(attrs, R.styleable.ArcBarView);
             arcBarHightToScreenPercentage = typedArray.getFloat(R.styleable.ArcBarView_arcBarHightToScreenPercentage, 0.0f);
             arcBarHightToArcPercentage = typedArray.getFloat(R.styleable.ArcBarView_arcBarHightToArcPercentage, 0.0f);
             arcBarWidthToArcPercentage = typedArray.getFloat(R.styleable.ArcBarView_arcBarWidthToArcPercentage, 0.0f);
-            startColor = typedArray.getColor(R.styleable.ArcBarView_startColor, 0);
-            centerColor = typedArray.getColor(R.styleable.ArcBarView_centerColor, 0);
-            endColor = typedArray.getColor(R.styleable.ArcBarView_endColor, 0);
+            arcBarColor = typedArray.getColor(R.styleable.ArcBarView_arcBarColor, arcBarColor);
+            isShowGradientColor = typedArray.getBoolean(R.styleable.ArcBarView_arcBarShowGradientColor, false);
+            startGradientColor = typedArray.getColor(R.styleable.ArcBarView_arcBarStartGradientColor, startGradientColor);
+            centerGradientColor = typedArray.getColor(R.styleable.ArcBarView_arcBarCenterGradientColor, centerGradientColor);
+            endGradientColor = typedArray.getColor(R.styleable.ArcBarView_arcBarEndGradientColor, endGradientColor);
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
@@ -60,14 +65,25 @@ public class ArcBarView extends View {
             }
         }
 
+        init();
+    }
 
+    private void init() {
         int heightPixels = getContext().getResources().getDisplayMetrics().heightPixels;
         float arcBarHight = heightPixels * arcBarHightToScreenPercentage;
         float value = arcBarHight * arcBarHightToArcPercentage;
-
         initPaint(arcBarHight);
         initPath(arcBarHight, value, arcBarWidthToArcPercentage);
+    }
 
+    public ArcBarView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        canvas.drawColor(Color.WHITE);
+        canvas.drawPath(path, paint);
     }
 
     private void initPath(float arcBarHight, float value, float widthPercentage) {
@@ -83,28 +99,31 @@ public class ArcBarView extends View {
         oval.right = widthPixels + widthPixels * widthPercentage;
         oval.bottom = arcBarHight;
         path.addArc(oval, 0, 180);
-
         path.setFillType(Path.FillType.WINDING);
     }
 
     private void initPaint(float arcBarHight) {
         paint = new Paint();
         paint.setAntiAlias(true);
-        Shader mShader = new LinearGradient(0, 0, 0, arcBarHight, getColors(), null, Shader.TileMode.MIRROR);
-        paint.setShader(mShader);
-        paint.setStrokeWidth((float) 1.0);
+        if (isShowGradientColor) {
+            Shader mShader = new LinearGradient(0, 0, 0, arcBarHight, getColors(), null, Shader.TileMode.MIRROR);
+            paint.setShader(mShader);
+        } else {
+            paint.setColor(arcBarColor);
+        }
+        paint.setStrokeWidth(10);
         paint.setStyle(Paint.Style.FILL_AND_STROKE);
     }
 
     private int[] getColors() {
-        if (startColor != 0) {
-            list.add(startColor);
+        if (startGradientColor != 0) {
+            list.add(startGradientColor);
         }
-        if (centerColor != 0) {
-            list.add(centerColor);
+        if (centerGradientColor != 0) {
+            list.add(centerGradientColor);
         }
-        if (endColor != 0) {
-            list.add(endColor);
+        if (endGradientColor != 0) {
+            list.add(endGradientColor);
         }
         int size = list.size();
         int[] colors = new int[size];
@@ -117,15 +136,5 @@ public class ArcBarView extends View {
             colors = defaultColors;
         }
         return colors;
-    }
-
-    public ArcBarView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
-        super(context, attrs, defStyleAttr);
-    }
-
-    @Override
-    protected void onDraw(Canvas canvas) {
-        canvas.drawColor(Color.WHITE);
-        canvas.drawPath(path, paint);
     }
 }
